1082f14b7a126cd36cf4b86fc5b192e043262703
ugo-studio <ugo.code.studio@gmail.com>
2025-08-02 13:51:43 +0100

    chore(example/windows): remove flutter_p2p_connection plugin

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <flutter_p2p_connection/flutter_p2p_connection_plugin_c_api.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FlutterP2pConnectionPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterP2pConnectionPluginCApi"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  flutter_p2p_connection
   permission_handler_windows
 )
 
